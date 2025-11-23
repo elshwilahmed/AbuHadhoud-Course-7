e036b4d83d55524df1f6e1dd4f04f0f9773e9fad
elshwilahmed <elshwilahmed@gmail.com>
2025-11-23 19:11:39 +0200

    Update 02_Sum_of_each_row.cpp

--- 02_Sum_of_each_row.cpp
+++ 02_Sum_of_each_row.cpp
@@ -1,10 +1,12 @@
+/*
+        Write a program to fill a 3x3 Matrix with random numbers and print the sum of each row
+*/
+
+
+
 #include <iostream>
 #include <bits/stdc++.h>
-// #include<ext/pb_ds/assoc_container.hpp>
-// #include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
-// using namespace __gnu_pbds;
-// typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 //*-------------------------------------------*
 void fastIO(void) {
