311153593877f67b1c5b481507ae75f01af04cc2
ahmed elshwil <elshwilahmed@gmail.com>
2025-11-28 21:25:10 +0200

    string

---
+++ 23_First_letter.cpp
@@ -0,0 +1,43 @@
+/*
+Write a program to print first letter of each word
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//*-------------------------------------------*
+void fastIO(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+//*-------------------------------------------*
+void PrintFirstCharInAllWords(string s) {
+    bool isfirst = true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] != ' ' && isfirst)
+            cout << s[i] << endl;
+        (s[i] == ' ') ? isfirst = true : isfirst = false;
+    }
+}
+
+void njr() {
+    string s;
+    cout << "Please Enter Your String: \n" << flush;
+    getline(cin, s);
+    PrintFirstCharInAllWords(s);
+}
+
+
+signed main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    fastIO();
+    int t = 1; //cin >> t;
+    while (t--) {
+        njr();
+    }
+    return 0;
+}
